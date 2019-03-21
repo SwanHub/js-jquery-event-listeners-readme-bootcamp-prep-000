225836a8e5d12c767baf6b082336bc47e01f6fc3
Jackson Prince <jdprince555@gmail.com>
2019-03-21 06:23:39 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input#typing').on('keydown', function(key) {
+  $('#typing').on('keydown', function(key) {
     if (key === "g") {
       alert("g key submission")
     }
